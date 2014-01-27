d44ca6259ea235488101efe97abda5b2a404a377
U-Tab\jaylu_000 <jaylu_000@Tab.(none)>
2014-01-27 14:32:19 -0800

    initial commit

---
+++ src/BasicCrawlController.java
@@ -0,0 +1,112 @@
+
+
+import java.util.List;
+
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+/**
+ * @author Yasser Ganjisaffar <lastname at gmail dot com>
+ */
+public class BasicCrawlController {
+
+		private static String 	USER_AGENT = 		"UCI IR 76111817";
+		private static String 	STORAGE_FOLDER = 	"/crawl/root/";
+		private static int 	  	CRAWL_THREADS = 	2;
+		private static int 	  	CRAWL_DELAY_MS = 	300;
+		private static int 	  	CRAWL_DEPTH = 		2;
+		private static int    	CRAWL_MAX_PG_TO_FETCH = 10;
+		private static boolean 	CRAWL_RESUMABLE = 	false;
+		private static String 	CRAWL_SEED = 		"http://www.ics.uci.edu/~lopes/";
+		
+        public static void main(String[] args) throws Exception {
+
+                /*
+                 * crawlStorageFolder is a folder where intermediate crawl data is
+                 * stored.
+                 */
+                String crawlStorageFolder = STORAGE_FOLDER;
+
+                /*
+                 * numberOfCrawlers shows the number of concurrent threads that should
+                 * be initiated for crawling.
+                 */
+                int numberOfCrawlers = CRAWL_THREADS;
+
+                CrawlConfig config = new CrawlConfig();
+
+                config.setCrawlStorageFolder(STORAGE_FOLDER);
+
+                /*
+                 * Be polite: Make sure that we don't send more than 1 request per
+                 * second (1000 milliseconds between requests).
+                 */
+                config.setPolitenessDelay(CRAWL_DELAY_MS);
+
+                /*
+                 * You can set the maximum crawl depth here. The default value is -1 for
+                 * unlimited depth
+                 */
+                config.setMaxDepthOfCrawling(CRAWL_DEPTH);
+
+                /*
+                 * You can set the maximum number of pages to crawl. The default value
+                 * is -1 for unlimited number of pages
+                 */
+                config.setMaxPagesToFetch(CRAWL_MAX_PG_TO_FETCH);
+
+                /*
+                 * This config parameter can be used to set your crawl to be resumable
+                 * (meaning that you can resume the crawl from a previously
+                 * interrupted/crashed crawl). Note: if you enable resuming feature and
+                 * want to start a fresh crawl, you need to delete the contents of
+                 * rootFolder manually.
+                 */
+                config.setResumableCrawling(CRAWL_RESUMABLE);
+                
+                //Set user agent
+                config.setUserAgentString(USER_AGENT);
+                
+                /*
+                 * Instantiate the controller for this crawl.
+                 */
+                PageFetcher pageFetcher = new PageFetcher(config);
+                RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+                RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+                CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+                /*
+                 * For each crawl, you need to add some seed urls. These are the first
+                 * URLs that are fetched and then the crawler starts following links
+                 * which are found in these pages
+                 */
+
+                //controller.addSeed("http://www.ics.uci.edu/");
+                controller.addSeed(CRAWL_SEED);
+
+                /*
+                 * Start the crawl. This is a blocking operation, meaning that your code
+                 * will reach the line after this only when crawling is finished.
+                 */
+                controller.start(BasicCrawler.class, numberOfCrawlers);
+                
+                //Calculate stats
+                List<Object> crawlersLocalData = controller.getCrawlersLocalData();
+                long totalLinks = 0;
+                long totalTextSize = 0;
+                int totalProcessedPages = 0;
+                for (Object localData : crawlersLocalData) {
+                        CrawlStat stat = (CrawlStat) localData;
+//                        totalLinks += stat.getTotalLinks();
+//                        totalTextSize += stat.getTotalTextSize();
+//                        totalProcessedPages += stat.getTotalProcessedPages();
+                }
+                System.out.println("Aggregated Statistics:");
+                System.out.println("   Processed Pages: " + totalProcessedPages);
+                System.out.println("   Total Links found: " + totalLinks);
+                System.out.println("   Total Text Size: " + totalTextSize);
+        }
+}
---
+++ src/CrawlStat.java
@@ -0,0 +1,142 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+import edu.uci.ics.crawler4j.url.WebURL;
+
+public class CrawlStat {
+        private int 					mNumPages;			//# of pages
+        private long 					mLongestPageLength; //# words in longest page
+        private Map<String, Integer> 	mSubdomainsMap;		//subdomain names and frequencies
+        private Map<String, Integer> 	mWordMap;			//words and frequencies Map
+        private Map<String, Integer> 	m2gramMap;			//2grams and frequencies Map
+        private int 					mMaxTextLength;		//Maximum text length
+
+        //Helper function for robust mapping of values
+        private void insertMap(Map<String, Integer> map, String word){
+        	Integer count = map.get(word);
+        	if(count == null)
+        		map.put(word, 1);
+        	else{
+        		map.put(word, count+1);
+        	}
+        }
+        
+        //Helper function sorts a map and returns top n values
+        private ArrayList<Frequency> topNMap(Map<String, Integer>freqMap, int topNvals){
+        	if(freqMap.size() < 1) return null;
+        	
+        	//Sort by frequency, then by alphabetical order
+    		Map.Entry<String, Integer>[] entries = freqMap.entrySet().toArray(new Map.Entry[0]);
+    		Arrays.sort(entries, new Comparator<Map.Entry<String, Integer>>() {
+                @Override
+                public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                    Integer v1 = o1.getValue();
+                	Integer v2 = o2.getValue();
+                    
+                    //First compare by frequency
+                    int comp = -1*v1.compareTo(v2);
+                    if(comp != 0){
+                    	return comp;
+                    } else {	//if that doesn't work, compare by alphabet
+                    	String k1 = o1.getKey();
+                    	String k2 = o2.getKey();
+                    	return k1.compareTo(k2);
+                    }
+                }
+            });
+    		
+    		//Create the list
+    		ArrayList<Frequency> wordList = new ArrayList<Frequency>();
+    		
+    		int counter = 0;
+    		for(Map.Entry<String, Integer> entry:entries){
+    			if(counter < topNvals)
+    				wordList.add(new Frequency(entry.getKey(), entry.getValue()));
+    			else break;
+    		}
+    		return wordList;
+        }
+        
+        public int getNumPages() {
+                return mNumPages;
+        }
+
+        public void incNumPages(int n){
+        	mNumPages += n;
+        }
+        
+        public long getLongestPageLength() {
+            return mLongestPageLength;
+        }
+        
+        public Map<String, Integer> getSubdomainsMap() {
+            return mSubdomainsMap;
+        }
+
+        //parses the url to its domain and subdomain and indexes its location
+        public void insertSubdomainsMap(WebURL url){
+        	String link = url.getDomain().toString() + url.getParentUrl().toString();
+        	insertMap(this.mSubdomainsMap, link);
+        }
+        
+        public Map<String, Integer> getWordMap() {
+            return mWordMap;
+        }
+        
+        //Adds a word into the word map
+        public void insertWordMap(String word){
+        	insertMap(this.mWordMap, word);
+        }
+        
+        //returns top n words
+        public ArrayList<Frequency> getTopNWords(int n){
+        	return topNMap(this.mWordMap, n);
+        }
+        
+        //returns 2gram map
+        public Map<String, Integer> get2gramMap() {
+            return m2gramMap;
+        }
+        
+        //Insert a word into the 2gram map
+        public void insert2gramMap(String word){
+        	insertMap(this.m2gramMap, word);
+        }
+        
+        //returns top n 2grams
+        public ArrayList<Frequency> getTopNGrams(int n){
+        	return topNMap(this.m2gramMap, n);
+        }
+        
+        public int getMaxTextLength(){
+        	return mMaxTextLength;
+        }
+        
+        //update the max text length
+        public void updateMaxTextLength(int n){
+        	if(n > mMaxTextLength) 
+        		mMaxTextLength = n;
+		}
+}
+		
